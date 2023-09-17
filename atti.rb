3807aee8a12c2ab2fbb2de8e30e6cd98c88994de
EC2 Default User <ec2-user@ip-172-31-42-129.ap-northeast-3.compute.internal>
2023-09-17 14:08:00 +0000

    あっち向いてホイ-attitwo

--- atti.rb
+++ atti.rb
@@ -1,84 +1,76 @@
       puts"じゃんけん..."
    
 class Attimuite_hoi
-  def jyanken
-      @jyanken_result=""
-      puts"0(グー)1(チョキ)2(パー)3(戦わない)を入力してください"
-    
-      player_hand=gets.to_i
-      computer_hand=rand(0..2)
-      hand_type=["グー","チョキ","パー"]
-    
-    if player_hand==3
-        puts"戦いません"
-        exit
+    def jyanken
+        @jyanken_result=""
+        puts"0(グー)1(チョキ)2(パー)3(戦わない)を入力してください"
         
-    elsif player_hand>3 
-     puts"入力が間違っています"
-     puts"じゃんけん..."
-     return true
-     
-    else 
-    
-      puts"ホイ！"
-      puts"-----------------------"
-      puts"あなた：#{hand_type[player_hand]}を出しました"
-      puts"相手：#{hand_type[computer_hand]}を出しました"
-      puts"-----------------------"
-    
-       if player_hand==computer_hand
-         puts"あいこで..."
-         return true
-    
-       elsif (player_hand==0 && computer_hand==1)||(player_hand==1 && computer_hand==2)||(player_hand==2 && computer_hand==0)
-          puts"あっち向いて〜"
-          @jyanken_result="win"
-          attimuite
-          return false
-          
-       else (player_hand==0 && computer_hand==2)||(player_hand==1 && computer_hand==0)||(player_hand==2 && computer_hand==1)
-          puts"あっち向いて〜"
-          @jyanken_result="lose"
-          attimuite
-          return false
-         
-       end 
+        player_hand=gets.to_i
+        computer_hand=rand(0..2)
+        hand_type=["グー","チョキ","パー"]
+        
+        if player_hand==3
+            puts"戦いません"
+            exit
+            
+        elsif player_hand>3
+            puts"入力が間違っています"
+            puts"じゃんけん..."
+            return true
+            
+        else
+            puts"ホイ！"
+            puts"-----------------------"
+            puts"あなた：#{hand_type[player_hand]}を出しました"
+            puts"相手：#{hand_type[computer_hand]}を出しました"
+            puts"-----------------------"
+            
+            if player_hand==computer_hand
+                puts"あいこで..."
+                return true
+            
+            elsif (player_hand==0 && computer_hand==1)||(player_hand==1 && computer_hand==2)||(player_hand==2 && computer_hand==0)
+                puts"あっち向いて〜"
+                @jyanken_result="win"
+                return attimuite
+                
+            else (player_hand==0 && computer_hand==2)||(player_hand==1 && computer_hand==0)||(player_hand==2 && computer_hand==1)
+                puts"あっち向いて〜"
+                @jyanken_result="lose"
+                return attimuite
+                
+            end 
+        end
     end
-    next_game = true
-
-    while next_game do
-        next_game = attimuite_hoi
-    end
     
-  end
-  
-  
-  def attimuite
-      puts"0(上)1(下)2(左)3(右)"
+    def attimuite
+        puts"0(上)1(下)2(左)3(右)"
+        
+        player_face=gets.to_i
+        computer_face=rand(0..3)
+        face_type=["上","下","左","右"]
+        
+        puts"ホイ！"
+        puts"-----------------------"
+        puts"あなた:#{face_type[player_face]}"
+        puts"相手:#{face_type[computer_face]}"
+        puts"-----------------------"
+        
+        if player_face==computer_face&&@jyanken_result=="win"
+            puts"あなたの勝ちです！"
+            exit
+        
+        elsif player_face==computer_face&&@jyanken_result=="lose"
+           puts"あなたの負けです"
+           exit 
+        
+        else
+            puts"じゃんけんに戻ります"
+            puts"じゃんけん..."
+            return true
+        end
+    end    
       
-      player_face=gets.to_i
-      computer_face=rand(0..3)
-      face_type=["上","下","左","右"]
-      
-      puts"ホイ！"
-      puts"-----------------------"
-      puts"あなた:#{face_type[player_face]}"
-      puts"相手:#{face_type[computer_face]}"
-      puts"-----------------------"
-      
-     if player_face==computer_face&&@jyanken_result=="win"
-         puts"あなたの勝ちです！"
-         exit
-         
-     elsif player_face==computer_face&&@jyanken_result=="lose"
-         puts"あなたの負けです"
-         exit
-     else 
-         puts"じゃんけんに戻ります"
-         puts"じゃんけん..."
-         return true
-     end    
-  end    
 end 
 
 
@@ -87,7 +79,7 @@ attimuite_hoi.jyanken
 attimuite_hoi.attimuite
 
 next_game = true
-
-while next_game do
-    next_game = attimuite_hoi
-end
+  while next_game do
+      next_game = attimuite_hoi.jyanken
+  end
+  
